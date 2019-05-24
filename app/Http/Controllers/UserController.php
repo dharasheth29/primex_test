a586f1e80cc7b9e81dfb168c0eef936200860af0
dharasheth29 <dhara.s.sheth@gmail.com>
2019-05-24 10:48:47 +1000

    Update file to use the latest version

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\UserRole;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -14,43 +17,88 @@ class UserController extends Controller
      */
     public function index()
     {
-
-        // $users = User::all();
-        $users = User::with('user_role')->get();
+        $users = DB::table('user_roles AS ur')
+            ->rightjoin('users AS u', 'u.role_id', 'ur.id')
+            ->select('u.id', 'u.firstname', 'u.lastname', 'u.company', 'u.email', 'ur.role')
+            ->get();
 
         return response()->json($users);
     }
 
     public function create(Request $request)
     {
-        $user = new User;
-        $user->firstname = $request->firstname;
-        $user->lastname = $request->lastname;
-        $user->email = $request->email;
-        $user->company = $request->company;
-        $user->role = $request->role;
+        $this->validate($request, [
+            'firstname' => 'required|string',
+            'lastname' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'company' => 'required|string|max:50',
+            'role' => 'required|string|max:50',
+        ]);
 
-        $user->save();
-        return response()->json($user);
+        $role = DB::table('user_roles AS ur')
+            ->select('ur.id')
+            ->where('ur.role', '=', strtoupper($request->role))
+            ->first();
+
+        if (empty($role)) {
+            return response()->json(['status' => 'error', 'message' => 'This role is not defined in our database.'], 401);
+        } else {
+            $user = new User;
+            $user->firstname = $request->firstname;
+            $user->lastname = $request->lastname;
+            $user->email = $request->email;
+            $user->company = $request->company;
+            $user->role_id = $role->id;
+
+            $user->save();
+
+            return response()->json($user);
+        }
     }
 
     public function show($id)
     {
-        $user = User::find($id);
+        $user = DB::table('user_roles AS ur')
+            ->rightjoin('users AS u', 'u.role_id', 'ur.id')
+            ->select('u.id', 'u.firstname', 'u.lastname', 'u.company', 'u.email', 'ur.role')
+            ->where('u.id', '=', $id)
+            ->get();
         return response()->json($user);
     }
 
     public function update(Request $request, $id)
     {
-        $user = User::find($id);
+        $this->validate($request, [
+            'firstname' => 'required|string',
+            'lastname' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'company' => 'required|string|max:50',
+            'role' => 'required|string|max:50',
+        ]);
 
-        $user->firstname = $request->input('firstname');
-        $user->lastname = $request->input('lastname');
-        $user->email = $request->input('email');
-        $user->company = $request->input('company');
-        $user->role = $request->input('role');
-        $user->save();
-        return response()->json($user);
+        $role = DB::table('user_roles AS ur')
+            ->select('ur.id')
+            ->where('ur.role', '=', strtoupper($request->role))
+            ->first();
+
+        if (empty($role)) {
+            return response()->json(['status' => 'error', 'message' => 'This role is not defined in our system.'], 401);
+        } else {
+            $user = User::find($id);
+            if (empty($user)) {
+                return response()->json(['status' => 'error', 'message' => 'This user does not exist in our system.'], 401);
+            } else {
+                $user->firstname = $request->firstname;
+                $user->lastname = $request->lastname;
+                $user->email = $request->email;
+                $user->company = $request->company;
+                $user->role_id = $role->id;
+
+                $user->save();
+
+                return response()->json($user);
+            }
+        }
     }
 
     public function destroy($id)
@@ -60,3 +108,4 @@ class UserController extends Controller
         return response()->json('user removed successfully');
     }
 }
+
